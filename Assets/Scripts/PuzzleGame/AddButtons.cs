6a3fd13aa7e04a9002de5db89fbb99c909d090ae
LaylaTD <2011419@cstj.qc.ca>
2023-02-22 21:59:34 -0500

    PuzzleGame

---
+++ Assets/Scripts/PuzzleGame/AddButtons.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddButtons : MonoBehaviour
+{
+    [SerializeField] private Transform puzzleField;
+    [SerializeField] private GameObject btn;
+
+    void Awake() {
+        
+    }
+}
