c21ded400d6b5cafd420a22d2fafeb8d79403ef6
CSTJ\2011419 <2011419@cstj.qc.ca>
2023-03-07 12:56:05 -0500

    Developpement Accueil

--- Assets/Scripts/ActiverGameObject.cs
+++ Assets/Scripts/ActiverGameObject.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ActiveGO : MonoBehaviour
+public class ActiverGameObject : MonoBehaviour
 {
     [SerializeField] private GameObject _GO;
 
